9bfc896a97a89374c6257050f36f412eb2f81aa5
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-06-20 10:30:56 +0200

    Nu med fyrværkeri

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import Reveal from "reveal.js";
 import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/sky.css'
+import 'reveal.js/dist/theme/night.css'
 import RevealNotes from 'reveal.js/plugin/notes/notes.esm'
 import confetti from 'canvas-confetti'
+import '/node_modules/flag-icons/css/flag-icons.min.css'
 
 const deck = new Reveal()
 
@@ -15,16 +16,9 @@ deck.initialize({
 
 deck.on('slidetransitionend', event => {
   let curSlide = deck.getIndices()
-  if (curSlide.h == 4 && curSlide.v == 1) {
-    // confetti({
-    //   spread: 180,
-    //   particleCount: 300
-    // });
+  if (curSlide.h == 8 && curSlide.v == 1) {
     var end = Date.now() + (10  * 1000);
-
-    // go Buckeyes!
-    var colors = ['#610469', '#ffffff'];
-
+    var colors = ['#79EBEA', '#53EB4F'];
     (function frame() {
       confetti({
         particleCount: 2,
@@ -45,5 +39,26 @@ deck.on('slidetransitionend', event => {
         requestAnimationFrame(frame);
       }
     }());
+  } else if (curSlide.h == 9 && curSlide.v == 0) {
+    var duration = 15 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+      confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 250);
   }
 })
